dbea2c05e240f086313fdc164faa28ec57ca5c31
t29mato <tomoya.matou@gmail.com>
2021-10-18 11:09:41 +0000

    refactor: remove binary type, add count to return value

--- src/circle.test.ts
+++ src/circle.test.ts
@@ -7,52 +7,67 @@ test('Circle size: 0', () => {
 })
 test('Circle size: 1', () => {
   const asis = circle(1)
-  const tobe = [[1]]
+  const tobe = {
+    count: 1,
+    data: [[1]],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 2', () => {
   const asis = circle(2)
-  const tobe = [
-    [1, 1],
-    [1, 1],
-  ]
+  const tobe = {
+    data: [
+      [1, 1],
+      [1, 1],
+    ],
+    count: 4,
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 3', () => {
   const asis = circle(3)
-  const tobe = [
-    [0, 1, 0],
-    [1, 1, 1],
-    [0, 1, 0],
-  ]
+  const tobe = {
+    count: 5,
+    data: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 4', () => {
-  expect(circle(4)).toStrictEqual([
-    [0, 0, 0, 0],
-    [0, 1, 1, 0],
-    [0, 1, 1, 0],
-    [0, 0, 0, 0],
-  ])
+  expect(circle(4)).toStrictEqual({
+    count: 4,
+    data: [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0],
+    ],
+  })
 })
 test('Circle size: 11', () => {
   const asis = circle(11)
-  const tobe = [
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  ]
+  const tobe = {
+    count: 81,
+    data: [
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
@@ -69,112 +84,135 @@ test('Circle size: 1, inline size: 0', () => {
 })
 
 test('Circle size: 1, inline size: 1', () => {
-  const tobe = [[1]]
-  expect(circleInline(1, 1)).toStrictEqual(tobe)
-})
-test('Circle size: 1, inline size: 1', () => {
-  const tobe = [[1]]
+  const tobe = {
+    count: 1,
+    data: [[1]],
+  }
   expect(circleInline(1, 1)).toStrictEqual(tobe)
 })
 
 test('Circle size: 3, inline size: 1', () => {
   const asis = circleInline(3, 1)
-  const tobe = [
-    [0, 1, 0],
-    [1, 0, 1],
-    [0, 1, 0],
-  ]
+  const tobe = {
+    count: 4,
+    data: [
+      [0, 1, 0],
+      [1, 0, 1],
+      [0, 1, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 4, inline size: 1', () => {
   const asis = circleInline(4, 1)
-  const tobe = [
-    [0, 0, 0, 0],
-    [0, 1, 1, 0],
-    [0, 1, 1, 0],
-    [0, 0, 0, 0],
-  ]
+  const tobe = {
+    count: 4,
+    data: [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 test('Circle size: 5, inline size: 1', () => {
   const asis = circleInline(5, 1)
-  const tobe = [
-    [0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0],
-    [1, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0],
-    [0, 0, 1, 0, 0],
-  ]
+  const tobe = {
+    count: 8,
+    data: [
+      [0, 0, 1, 0, 0],
+      [0, 1, 0, 1, 0],
+      [1, 0, 0, 0, 1],
+      [0, 1, 0, 1, 0],
+      [0, 0, 1, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 5, inline size: 2', () => {
   const asis = circleInline(5, 2)
-  const tobe = [
-    [0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0],
-    [1, 1, 0, 1, 1],
-    [0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-  ]
+  const tobe = {
+    count: 12,
+    data: [
+      [0, 0, 1, 0, 0],
+      [0, 1, 1, 1, 0],
+      [1, 1, 0, 1, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 5, inline size: 3', () => {
   const asis = circleInline(5, 3)
-  const tobe = [
-    [0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-  ]
+  const tobe = {
+    count: 13,
+    data: [
+      [0, 0, 1, 0, 0],
+      [0, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 test('Circle size: 5, inline size: 4', () => {
   const asis = circleInline(5, 4)
-  const tobe = [
-    [0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-  ]
+  const tobe = {
+    count: 13,
+    data: [
+      [0, 0, 1, 0, 0],
+      [0, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 10, inline size: 2', () => {
   const asis = circleInline(10, 2)
-  const tobe = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]
+  const tobe = {
+    count: 44,
+    data: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
 
 test('Circle size: 11, inline size: 2', () => {
   const asis = circleInline(11, 2)
-  const tobe = [
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  ]
+  const tobe = {
+    count: 52,
+    data: [
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+      [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+      [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+      [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    ],
+  }
   expect(asis).toStrictEqual(tobe)
 })
--- src/circle.ts
+++ src/circle.ts
@@ -1,32 +1,46 @@
-type binary = 0 | 1
+interface result {
+  count: number
+  data: number[][]
+}
 
-export const circle = (size: number): binary[][] => {
+export const circle = (size: number): result => {
   if (size < 1) {
     throw new Error('Circle size should be more than 1')
   }
   if (size === 1) {
-    return [[1]]
+    return {
+      count: 1,
+      data: [[1]],
+    }
   }
   if (size === 2) {
-    return [
-      [1, 1],
-      [1, 1],
-    ]
+    return {
+      count: 4,
+      data: [
+        [1, 1],
+        [1, 1],
+      ],
+    }
   }
   const diameter = size - 1
   const radius = diameter / 2
-  const result = [...Array(size)].map(() => Array(size).fill(0))
+  const data = [...Array(size)].map(() => Array(size).fill(0))
+  let count = 0
   for (let h = 0; h < size; h++) {
     for (let w = 0; w < size; w++) {
       if ((h - radius) ** 2 + (w - radius) ** 2 <= radius ** 2) {
-        result[h][w] = 1
+        count++
+        data[h][w] = 1
       }
     }
   }
-  return result
+  return {
+    count,
+    data,
+  }
 }
 
-export const circleInline = (size: number, inlineSize: number): binary[][] => {
+export const circleInline = (size: number, inlineSize: number): result => {
   if (size < 1 || inlineSize < 1) {
     throw new Error('Circle size or inline width should be more than 1')
   }
@@ -41,14 +55,19 @@ export const circleInline = (size: number, inlineSize: number): binary[][] => {
     return circle(size)
   }
 
-  const result = [...Array(size)].map(() => Array(size).fill(0))
+  const data = [...Array(size)].map(() => Array(size).fill(0))
+  let count = 0
   for (let h = 0; h < size; h++) {
     for (let w = 0; w < size; w++) {
       const lengthSquare = (h - outerRadius) ** 2 + (w - outerRadius) ** 2
       if (lengthSquare <= outerRadius ** 2 && lengthSquare > innerRadius ** 2) {
-        result[h][w] = 1
+        count++
+        data[h][w] = 1
       }
     }
   }
-  return result
+  return {
+    count,
+    data,
+  }
 }
