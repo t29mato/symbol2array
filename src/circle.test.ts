dff0e2681ad57e43c2d340ac4db7caa46b3364db
t29mato <tomoya.matou@gmail.com>
2021-10-18 08:12:55 +0000

    fix(circleOutline): pass for other test cases

--- src/circle.test.ts
+++ src/circle.test.ts
@@ -41,12 +41,90 @@ test('diameter is 4', () => {
     )
 })
 
-test('diameter is 3', () => {
+test('circle outline: length 0, outline width: 0', () => {
+    expect(() => {
+        circleOutline2array(0, 0)
+    }).toThrowError('Circle length or outline width should be more than 1')
+})
+
+test('circle outline: length 1, outline width: 0', () => {
+    expect(() => {
+        circleOutline2array(1, 0)
+    }).toThrowError('Circle length or outline width should be more than 1')
+})
+
+test('circle outline: length 1, outline width: 1', () => {
+    const tobe = [[1]]
+    expect(circleOutline2array(1, 1)).toStrictEqual(tobe)
+})
+test('circle outline: length 1, outline width: 1', () => {
+    const tobe = [[1]]
+    expect(circleOutline2array(1, 1)).toStrictEqual(tobe)
+})
+
+test('circle outline: length 3, outline width: 1', () => {
     const asis = circleOutline2array(3, 1)
     const tobe = [
         [ 0, 1, 0 ],
         [ 1, 0, 1 ],
         [ 0, 1, 0 ]
+    ]
+    expect(asis).toStrictEqual(tobe)
+})
+
+test('circle outline: length 4, outline width: 1', () => {
+    const asis = circleOutline2array(4, 1)
+    const tobe = [
+        [ 0, 0, 0, 0 ],
+        [ 0, 1, 1, 0 ],
+        [ 0, 1, 1, 0 ],
+        [ 0, 0, 0, 0 ]
+    ]
+    expect(asis).toStrictEqual(tobe)
+})
+test('circle outline: length 5, outline width: 1', () => {
+    const asis = circleOutline2array(5, 1)
+    const tobe = [
+        [ 0, 0, 1, 0, 0 ],
+        [ 0, 1, 0, 1, 0 ],
+        [ 1, 0, 0, 0, 1 ],
+        [ 0, 1, 0, 1, 0 ],
+        [ 0, 0, 1, 0, 0 ],
+    ]
+    expect(asis).toStrictEqual(tobe)
+})
+
+test('circle outline: length 5, outline width: 2', () => {
+    const asis = circleOutline2array(5, 2)
+    const tobe = [
+        [ 0, 0, 1, 0, 0 ],
+        [ 0, 1, 1, 1, 0 ],
+        [ 1, 1, 0, 1, 1 ],
+        [ 0, 1, 1, 1, 0 ],
+        [ 0, 0, 1, 0, 0 ],
+    ]
+    expect(asis).toStrictEqual(tobe)
+})
+
+test('circle outline: length 5, outline width: 3', () => {
+    const asis = circleOutline2array(5, 3)
+    const tobe = [
+        [ 0, 0, 1, 0, 0 ],
+        [ 0, 1, 1, 1, 0 ],
+        [ 1, 1, 1, 1, 1 ],
+        [ 0, 1, 1, 1, 0 ],
+        [ 0, 0, 1, 0, 0 ],
+    ]
+    expect(asis).toStrictEqual(tobe)
+})
+test('circle outline: length 5, outline width: 4', () => {
+    const asis = circleOutline2array(5, 4)
+    const tobe = [
+        [ 0, 0, 1, 0, 0 ],
+        [ 0, 1, 1, 1, 0 ],
+        [ 1, 1, 1, 1, 1 ],
+        [ 0, 1, 1, 1, 0 ],
+        [ 0, 0, 1, 0, 0 ],
     ]
     expect(asis).toStrictEqual(tobe)
 })
--- src/circle.ts
+++ src/circle.ts
@@ -27,16 +27,17 @@ export const circleOutline2array = (length: number, outlineWdith: number): binar
     if (length < 1 || outlineWdith < 1) {
         throw new Error("Circle length or outline width should be more than 1");
     }
-    if (length === 1) {
-        return [[1]]
-    }
-    if (length === 2) {
-        return [[ 1, 1], [ 1, 1]]
+    if (length < 3) {
+        return circle2array(length)
     }
     const outerDiameter = length - 1
     const outerRadius = outerDiameter / 2
     const innerDiameter = outerDiameter - outlineWdith * 2
     const innerRadius = innerDiameter / 2
+    if (outerDiameter <= innerDiameter || innerDiameter < 0) {
+        return circle2array(length)
+    }
+
     const result = [...Array(length)].map(item => Array(length).fill(0))
     for (let h = 0; h < length; h++) {
         for (let w = 0; w < length; w++) {
