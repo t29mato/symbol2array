773a38af8bc581859aa3b9ea3bc5c0df9c34da1c
t29mato <tomoya.matou@gmail.com>
2021-10-18 12:40:07 +0000

    fix: circle was small

--- src/circle.ts
+++ src/circle.ts
@@ -1,3 +1,4 @@
+// TODO: resultでは名前がいけてない
 interface result {
   count: number
   data: number[][]
@@ -28,7 +29,7 @@ export const circle = (size: number): result => {
   let count = 0
   for (let h = 0; h < size; h++) {
     for (let w = 0; w < size; w++) {
-      if ((h - radius) ** 2 + (w - radius) ** 2 <= radius ** 2) {
+      if ((h - radius) ** 2 + (w - radius) ** 2 <= radius ** 2.1) {
         count++
         data[h][w] = 1
       }
@@ -60,7 +61,10 @@ export const circleInline = (size: number, inlineSize: number): result => {
   for (let h = 0; h < size; h++) {
     for (let w = 0; w < size; w++) {
       const lengthSquare = (h - outerRadius) ** 2 + (w - outerRadius) ** 2
-      if (lengthSquare <= outerRadius ** 2 && lengthSquare > innerRadius ** 2) {
+      if (
+        lengthSquare <= outerRadius ** 2.1 &&
+        lengthSquare > innerRadius ** 2.1
+      ) {
         count++
         data[h][w] = 1
       }
