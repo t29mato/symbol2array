db1aa7960cb5b24ed86f9ba9509fb51880397505
t29mato <tomoya.matou@gmail.com>
2021-10-26 05:57:18 +0000

    fix: forgot to export TriangleCreator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { CircleCreator } from './circle'
-import { SquareCreator } from './square'
-import { DiamondCreator } from './diamond'
+import { CircleCreator } from 'circle'
+import { SquareCreator } from 'square'
+import { DiamondCreator } from 'diamond'
+import { TriangleCreator } from 'triangle'
 
-export { CircleCreator, SquareCreator, DiamondCreator }
+export { CircleCreator, SquareCreator, DiamondCreator, TriangleCreator }
