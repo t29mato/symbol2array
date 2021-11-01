b2fb3b9a93b8f10e3546800d2d810e19b521cfbf
t29mato <tomoya.matou@gmail.com>
2021-11-01 01:32:17 +0000

    fix: triagnle above left & right side was wrong

--- src/triangle/triangle.ts
+++ src/triangle/triangle.ts
@@ -8,45 +8,40 @@ export class Triangle extends SymbolClass {
   toArray(): { count: number; data: number[][] } {
     const data = this.create2DArray(this.arraySize, 0)
     let count = 0
-    const triangleSideLength = this.arraySize - 1
-    const halfSideLength = triangleSideLength / 2
-    const triangleHeight = halfSideLength * Math.sqrt(3)
+    const side = this.arraySize - 1
+    const halfSide = side / 2
+    const height = halfSide * Math.sqrt(3)
+    const border = this.inlineSize
 
-    // const diagonal = this.arraySize - 1
-    // const halfD = diagonal / 2
-    // const innerHalfD = halfD - this.inlineSize
-    const isInline = this.inlineSize > 0
-    for (let h = 0; h < this.arraySize; h++) {
-      for (let w = 0; w < this.arraySize; w++) {
-        const x = 0 - halfSideLength + w
-        const y = 0 + triangleHeight - h
-        const root3 = Math.sqrt(3)
-        const root2 = Math.sqrt(2)
+    const isInline = border > 0
+    // INFO: https://miro.com/app/board/o9J_lm3yQfU=/?moveToWidget=3074457366733302565&cot=14
+    for (let row = 0; row < this.arraySize; row++) {
+      for (let col = 0; col < this.arraySize; col++) {
+        // INFO: 原点は外側の三角形の下辺の中心
+        const x = col - halfSide
+        const y = height - row
         // INFO: 底辺より下は対象外
         if (y < 0) {
           continue
         }
         if (isInline) {
-          if (root3 * (halfSideLength - Math.abs(x)) < y) {
+          if (y > -2 * Math.abs(x) + height) {
             continue
           }
-          if (
-            root3 * (halfSideLength - Math.abs(x)) - this.inlineSize * root2 >=
-            y
-          ) {
-            if (y > this.inlineSize) {
+          if (y < -2 * Math.abs(x) + height - 2 * border) {
+            if (y > border) {
               continue
             }
           }
           count++
-          data[h][w] = 1
+          data[row][col] = 1
         } else {
           // INFO: 左右上辺は対象外
-          if (root3 * (halfSideLength - Math.abs(x)) < y) {
+          if (y > -2 * Math.abs(x) + height) {
             continue
           }
           count++
-          data[h][w] = 1
+          data[row][col] = 1
         }
       }
     }
